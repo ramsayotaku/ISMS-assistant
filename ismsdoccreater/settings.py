501907d30984ca30220141b2d299fadb84cb8397
ramsayotaku <ramsay007otaku@gmail.com>
2025-12-04 14:34:03 +0545

    .env is created to hide credentials

--- ismsdoccreater/settings.py
+++ ismsdoccreater/settings.py
@@ -12,17 +12,19 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#c8@_gu_a@lignb1cuevk_+u*fb3(!-o2g&^@c10gxl=#4kv)2'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -123,5 +125,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-OPENAI_API_KEY = "YOUR_KEY"
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 
